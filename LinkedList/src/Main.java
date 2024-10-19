f113f75f1f47f1429b0f4cf604dda42de08a5f72
taolu-career <taolu.career@gmail.com>
2024-10-18 23:38:48 -0400

    Second commitment

--- LinkedList/src/Main.java
+++ LinkedList/src/Main.java
@@ -1,5 +1,22 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello Blind5");
+
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if (head == null) return null;
+        ListNode p = new ListNode();
+        while(p !== null){
+            p = head.next;
+            p.next = head;
+            head.next = null;
+            p = p.next;
+        }
+        return p.next;
     }
+}
+
+ class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
 }
