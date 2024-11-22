96b85a0e64d93d506b221c9269afc870a1ca1dd8
taolu-career <taolu.career@gmail.com>
2024-11-21 20:49:43 -0500

    leecode

--- SlidingWindows/src/LengthofLongestSubarrayWithatMostKFrequency.java
+++ SlidingWindows/src/LengthofLongestSubarrayWithatMostKFrequency.java
@@ -1,4 +1,32 @@
 package src;
 
+import java.util.HashMap;
+
 public class LengthofLongestSubarrayWithatMostKFrequency {
+    public int maxSubarrayLength(int[] nums, int k) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+
+        int l = 0;
+        int r = 0;
+        int count = 0;
+        while(r < nums.length){
+            map.put(nums[r],map.getOrDefault(nums[r],0) + 1);
+
+            while(map.get(nums[r]) > k){
+                int temp = map.get(nums[l]);
+                int v = temp - 1;
+                if(v == 0){
+                    map.remove(nums[l]);
+                }else{
+                    map.put(nums[l],v);
+                }
+                l++;
+            }
+
+            count = Math.max(r - l + 1,count);
+            r++;
+        }
+
+        return count;
+    }
 }
--- SlidingWindows/src/SubarrayProductLessThanK.java
+++ SlidingWindows/src/SubarrayProductLessThanK.java
@@ -1,4 +1,22 @@
 package src;
 
 public class SubarrayProductLessThanK {
+    public int numSubarrayProductLessThanK(int[] nums, int k) {
+        int l = 0;
+        int r = 0;
+        int s = 1;
+        int count = 0;
+        while(r < nums.length){
+            s *= nums[r];
+
+            while(s >= k && l <= r){
+                s = s / nums[l];
+                l++;
+            }
+            count += r - l + 1;
+            r++;
+
+        }
+        return count;
+    }
 }
