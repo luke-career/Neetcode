22853882f200858fdcf56d693c45432a0d83549d
taolu-career <taolu.career@gmail.com>
2025-01-15 18:32:54 -0500

    leetcode

--- Array_Hash/src/RemoveElement.java
+++ Array_Hash/src/RemoveElement.java
@@ -12,3 +12,19 @@ public class RemoveElement {
         return l ;
     }
 }
+
+
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int l = 0;
+        int r = 0;
+        while(r < nums.length){
+            if(nums[r] != val){
+                nums[l] = nums[r];
+                l++;
+            }
+            r++;
+        }
+        return l;
+    }
+}
