74a488ce279166b2217148ead7ed3c813d025458
taolu-career <taolu.career@gmail.com>
2025-01-18 21:21:37 -0500

    leetcode

--- TwoPointer/src/ReverseString.java
+++ TwoPointer/src/ReverseString.java
@@ -1,16 +1,16 @@
-package src;
 
-public class ReverseString {
+class ReverseString {
     public void reverseString(char[] s) {
-        int len = s.length;
         int l = 0;
-        int r = len - 1;
+        int r = s.length - 1;
         while(l < r){
-            char temp = s[r];
-            s[r] = s[l];
-            s[l] = temp;
+            char temp = s[l];
+            temp = s[l];
+            s[l] = s[r];
+            s[r] = temp;
             l++;
             r--;
         }
     }
 }
+
