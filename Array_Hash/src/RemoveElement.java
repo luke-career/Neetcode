2ab600f1380e5039f362408c31ee2799cc70fa51
taolu-career <taolu.career@gmail.com>
2025-02-19 23:51:20 -0500

    leetcode

--- Array_Hash/src/RemoveElement.java
+++ Array_Hash/src/RemoveElement.java
@@ -21,8 +21,10 @@ class Solution {
         while(r < nums.length){
             if(nums[r] != val){
                 nums[l] = nums[r];
+
                 l++;
             }
+
             r++;
         }
         return l;
