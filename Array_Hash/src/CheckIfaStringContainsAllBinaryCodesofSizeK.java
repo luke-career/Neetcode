9aa4012d2cd0fe787a24965ddc1b5cd21be9764e
taolu-career <taolu.career@gmail.com>
2025-03-12 21:03:43 -0400

    leetcode

--- Array_Hash/src/CheckIfaStringContainsAllBinaryCodesofSizeK.java
+++ Array_Hash/src/CheckIfaStringContainsAllBinaryCodesofSizeK.java
@@ -5,12 +5,11 @@ public class CheckIfaStringContainsAllBinaryCodesofSizeK {
         HashSet<String> hs = new HashSet<>();
         for(int i = 0; i+k <= s.length();i++){
             String sb = s.substring(i,i+k);
-            hs.add(sb);
 
+            hs.add(sb);
         }
         if(hs.size() == 1 << k ){
             return true;
-
         }
         return false;
     }
