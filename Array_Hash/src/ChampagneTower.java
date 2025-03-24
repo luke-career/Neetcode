2e7686505f3b5a7c9c2ea120b6e5752bd6840df6
taolu-career <taolu.career@gmail.com>
2025-03-23 22:31:50 -0400

    leetcode

--- Array_Hash/src/ChampagneTower.java
+++ Array_Hash/src/ChampagneTower.java
@@ -7,6 +7,7 @@ public class ChampagneTower {
                 if(res[i][j] > 1 ){
                     res[i+1][j+1] += (res[i][j] - 1) / 2;
                     res[i+1][j] += (res[i][j] - 1)/2;
+
                     res[i][j] = 1;
                 }
             }
