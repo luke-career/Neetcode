4ac8a75d4fab427a7727b5b6d25cb3bfb7aa7524
taolu-career <taolu.career@gmail.com>
2025-05-18 23:57:32 -0400

    leetcoe

--- Array_Hash/src/ConvertanArrayIntoa2DArrayWithConditions.java
+++ Array_Hash/src/ConvertanArrayIntoa2DArrayWithConditions.java
@@ -30,6 +30,8 @@ public class ConvertanArrayIntoa2DArrayWithConditions {
                 break;
             }
             res.add(list);
+            
+            
 
         }
         return res;
